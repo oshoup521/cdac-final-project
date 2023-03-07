210f89bc4c2193ac71571445ceda71133fb047ae
Osho Upadhyay <oshoup521@gmail.com>
2023-03-07 18:56:45 +0530

    added header pages

--- the_farmer's_mart/frontend/src/pages/RoleNav.jsx
+++ the_farmer's_mart/frontend/src/pages/RoleNav.jsx
@@ -1,4 +1,5 @@
 import AdminHeader from "./AdminHeader";
+import SupplierHeader from "./SupplierHeader";
 import DeliveryPersonHeader from "./DeliveryPersonHeader";
 import HeaderUser from "./HeaderUser";
 import NormalHeader from "./NormalHeader";
@@ -7,11 +8,14 @@ const RoleNav = () => {
   const user = JSON.parse(sessionStorage.getItem("active-user"));
   const admin = JSON.parse(sessionStorage.getItem("active-admin"));
   const deliveryPerson = JSON.parse(sessionStorage.getItem("active-delivery"));
+  const supplier = JSON.parse(sessionStorage.getItem("active-supplier"));
 
   if (user != null) {
     return <HeaderUser />;
   } else if (admin != null) {
     return <AdminHeader />;
+  } else if (supplier != null) {
+    return <SupplierHeader />;
   } else if (deliveryPerson != null) {
     return <DeliveryPersonHeader />;
   } else {
